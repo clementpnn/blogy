f1f811faf38bf17697937072167d68ad5582900c
Clément <clementpnc@gmail.com>
2022-11-18 10:26:40 +0100

    feat: add database

---
+++ database/dump.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS User
+(
+    id        INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username  VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+    admin     BOOLEAN      NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Post
+(
+    id      INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT         NOT NULL,
+    author  INT          NOT NULL,
+    title   VARCHAR(255) NOT NULL,
+    date    DATETIME     NOT NULL,
+    image   VARCHAR(255) NOT NULL,
+
+    FOREIGN KEY (author) REFERENCES User (id)
+);
+
+CREATE TABLE IF NOT EXISTS Comment
+(
+  id      INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  content TEXT         NOT NULL,
+  author  INT          NOT NULL,
+  post    INT,
+  comment INT,
+  date    DATETIME     NOT NULL,
+  
+  FOREIGN KEY (post)    REFERENCES Post    (id),
+  FOREIGN KEY (comment) REFERENCES Comment (id)
+);
